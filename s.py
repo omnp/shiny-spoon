6205fa573330331bac0f16bda9bd6fcafb18acbe
Olli M. <40587954+omnp@users.noreply.github.com>
2025-07-30 22:44:10 +0300

    Update s.py
    Convert from recursion to iteration on a list ("stack").

--- s.py
+++ s.py
@@ -31,93 +31,103 @@ if __name__ == '__main__':
 
     def rec(xs, s=None):
         global counter
-        counter += 1
         if s is None:
             s = set()
         xs_orig = xs
         s_orig = s
-        xs = sat.exclude(set(xs_orig), s_orig)
-        if not xs:
-            return s
-        if not all(xs):
-            return None
-        vs = sat.get_literals(xs)
-        v = None
-        for e in vs:
-            if -e not in vs:
-                if v is None or abs(e) < abs(v):
-                    v = e
-        if v is None:
-            for e in vs:
-                if v is None or abs(e) < abs(v):
-                    v = e
-        vs = [v] + ([] if -v not in vs else [-v])
-        for v in vs:
-            from_v = []
-            to_v = []
-            xs = sat.exclude(set(xs_orig), s_orig)
-            for x in xs:
-                if -v in x:
-                    if x != (-v,):
-                        from_v.append(tuple(e for e in x if e != -v))
-                if v in x:
-                    if x != (v,):
-                        to_v.append(set(-e for e in x if e != v))
+        s_list = [(s, 0)]
+        s_all = [s]
+        while s_list:
+            counter += 1
+            s_orig, v = s_list.pop()
+            xs = set(xs_orig)
+            s = s_orig
+            value, s, _, xs = sat.propagate(xs, s)
             print("v", v)
-            # print("from_v", from_v)
-            # print("to_v", to_v)
-            if to_v:
-                for elems in to_v:
-                    xs_ = sat.exclude(set(xs_orig), s_orig).union(from_v)
-                    s_ = set(s_orig).union({v})
-                    xs = xs_
-                    from_from_v = set()
-                    for fr in from_v:
-                        t = set()
-                        for e in fr:
+            if not xs:
+                assert not any(-e in s for e in s)
+                return s
+            if not all(xs):
+                continue
+            vs = sat.get_literals(xs)
+            v = None
+            for e in vs:
+                if -e not in vs:
+                    if v is None or abs(e) < abs(v):
+                        v = e
+            if v is None:
+                for e in vs:
+                    if v is None or abs(e) < abs(v):
+                        v = e
+            vs = [v] + ([] if -v not in vs else [-v])
+            xs__ = set(xs)
+            s__ = set(s)
+            for v in vs:
+                from_v = []
+                to_v = []
+                xs = set(xs__)
+                for x in xs:
+                    if -v in x:
+                        if x != (-v,):
+                            from_v.append(tuple(e for e in x if e != -v))
+                    if v in x:
+                        if x != (v,):
+                            to_v.append(set(-e for e in x if e != v))
+                # print("v", v)
+                # print("from_v", from_v)
+                # print("to_v", to_v)
+                if to_v:
+                    for elems in reversed(to_v):
+                        xs_ = set(xs__).union(from_v)
+                        s_ = set(s__).union({v})
+                        xs = xs_
+                        from_from_v = set()
+                        for fr in from_v:
+                            t = set()
+                            for e in fr:
+                                for x in xs:
+                                    if -e in x:
+                                        t = t.union((f for f in x if f != -e))
+                            if t:
+                                from_from_v.add(tuple(sorted(t, key=abs)))
+                        to_to_v = set()
+                        for e in elems:
+                            t = set()
                             for x in xs:
-                                if -e in x:
-                                    t = t.union((f for f in x if f != -e))
-                        if t:
-                            from_from_v.add(tuple(sorted(t, key=abs)))
-                    to_to_v = set()
-                    for e in elems:
-                        t = set()
+                                if e in x:
+                                    t = t.union((-f for f in x if f != e))
+                            if t:
+                                to_to_v.add(tuple(sorted(t, key=abs)))
+                        # print("elems", elems)
+                        # print("from_from_v", from_from_v)
+                        # print("to_to_v", to_to_v)
+                        xs = xs_.union({(e,) for e in elems}).union(from_from_v).union(to_to_v)
+                        s = set(s_)
                         for x in xs:
-                            if e in x:
-                                t = t.union((-f for f in x if f != e))
-                        if t:
-                            to_to_v.add(tuple(sorted(t, key=abs)))
-                    # print("elems", elems)
-                    # print("from_from_v", from_from_v)
-                    # print("to_to_v", to_to_v)
-                    xs = xs_.union({(e,) for e in elems}).union(from_from_v).union(to_to_v)
-                    s = set(s_)
+                            if len(x) <= 1:
+                                s = s.union(set(x))
+                        s = s.union(elems)
+                        value, s, _, xs = sat.propagate(xs, s)
+                        if value is False:
+                            continue
+                        del xs_
+                        del xs
+                        if s not in s_all:
+                            s_list.append((s, v))
+                            s_all.append(s)
+                else:
+                    xs = set(xs__).union(from_v)
+                    s = set(s__).union({v})
                     for x in xs:
                         if len(x) <= 1:
                             s = s.union(set(x))
-                    s = s.union(elems)
                     value, s, _, xs = sat.propagate(xs, s)
                     if value is False:
                         continue
-                    del xs_
                     del xs
-                    r = rec(xs_orig, s)
-                    if r is not None and not any(-e in r for e in r):
-                        return r
-            else:
-                xs = sat.exclude(set(xs_orig), s_orig).union(from_v)
-                s = set(s_orig).union({v})
-                for x in xs:
-                    if len(x) <= 1:
-                        s = s.union(set(x))
-                value, s, _, xs = sat.propagate(xs, s)
-                if value is False:
-                    continue
-                del xs
-                r = rec(xs_orig, s)
-                if r is not None and not any(-e in r for e in r):
-                    return r
+                    if s not in s_all:
+                        s_list.append((s, v))
+                        s_all.append(s)
         return None
 
     r = rec(set(xs))
