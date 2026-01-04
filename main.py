a13a3b75bb3b9a1196daa1da085a91eda6bc3908
Olli M. <40587954+omnp@users.noreply.github.com>
2026-01-04 08:53:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,25 +5,80 @@ import sat
 import resolution
 
 random.seed(1)
-if len(sys.argv) > 1:
-    if len(sys.argv) > 2:
-        m, n = sys.argv[1:3]
-        m, n = int(m), int(n)
-        clauses = php.php(m, n)
-        print('PHP:{}x{}'.format(m, n))
-        print(len(clauses))
-        variables = sat.get_variables(clauses)
+split = False
+args = list(sys.argv[1:])
+if args and "split" in args:
+    args.remove("split")
+    split = True
+if len(args) > 1:
+    m, n = args[0:2]
+    m, n = int(m), int(n)
+    clauses = php.php(m, n)
+    print('PHP:{}x{}'.format(m, n))
+    print(len(clauses))
+    variables = sat.get_variables(clauses)
+    print(len(clauses), len(variables))
+elif len(args) > 0:
+    filename = args[0]
+    with open(filename) as file:
+        import dimacs
+        text = file.read()
+        variables, clauses = dimacs.parse_dimacs(text)
+        clauses = {sat.clause(c) for c in clauses}
+        m = len(clauses)
         print(len(clauses), len(variables))
-    else:
-        filename = sys.argv[1]
-        with open(filename) as file:
-            import dimacs
-            text = file.read()
-            variables, clauses = dimacs.parse_dimacs(text)
-            clauses = {sat.clause(c) for c in clauses}
-            m = len(clauses)
-            print(len(clauses), len(variables))
-            print('k', max(len(c) for c in clauses))
-    resolution.counter = 0
-    print(resolution.symmetry_breaking(clauses))
-    print(resolution.counter)
+        print('k', max(len(c) for c in clauses))
+else:
+    import math
+    random.seed(1)
+    ratio = 4.27
+    n = 110
+    m = int(math.ceil(n * ratio))
+    k = 3
+    h = 3  # <= Number of satisfying assignments
+
+    from sat import generate_assignment, randomize
+    # s = {sat.clause(generate_assignment(n)) for _ in range(h)}
+    # _, xs = resolution.random_instance_given_assignments(n, m, k, s)
+    # _, xs = resolution.random_instance_given_assignments(n, None, k, s)
+    # _, xs = resolution.random_instance_given_assignments(n, None, k, s, clustered=True)
+    # _, xs = resolution.random_instance_given_assignments(n, m, k)
+    # _, xs = resolution.random_instance_given_assignments(n, None, k)
+    _, xs = resolution.random_instance_given_assignments(n, None, k, clustered=True)
+    xs = randomize(xs)
+    clauses = xs
+xs = clauses
+resolution.counter = 0
+total = 0
+vs = sat.get_variables(xs)
+rs_ = []
+additional_xs = set()
+k = 3
+
+while True:
+    counter_ = resolution.counter
+    r = resolution.symmetry_breaking(xs, additional_xs)
+    if r is not None:
+        r = {e for e in r if abs(e) in vs}
+        for v in vs:
+            if -v not in r:
+                r.add(v)
+            if v not in r:
+                r.add(-v)
+        total += 1
+    print("\n\t", r is not None, all(r is None or any(e in r for e in x) for x in xs), resolution.counter - counter_, total)
+    if r is None:
+        break
+    r = sat.clause(-e for e in r if abs(e) in vs)
+    if split:
+        r = list(r)
+        random.shuffle(r)
+        r = sat.clause(r[:k])
+    for x in xs.union(additional_xs):
+        y = sat.resolve(r, x)
+        if y is not None and len(y) < len(r):
+            if y not in xs.union(additional_xs):
+                r = y
+    xs.add(r)
+    if r not in rs_:
+        rs_.append(r)
